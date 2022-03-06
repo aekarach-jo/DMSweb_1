4b848882ec1c8377476c9cc2845bb0795b080908
aekarach-jo <aekarach.jomkhamsing@gmail.com>
2022-03-07 02:53:00 +0700

    create invoice data

--- src/app/Services/api.service.ts
+++ src/app/Services/api.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { invoice } from '../Models/invoice';
 import { room } from '../Models/room';
 import { user } from '../Models/user';
+import { userDetail } from '../Models/userDetail';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +12,7 @@ import { user } from '../Models/user';
 export class ApiService {
 
   constructor(public http: HttpClient) { }
+
 
   public getAllUser() {
     return this.http.get<user>(`${environment.apiUrl}User/GetAllUser`)
@@ -29,6 +32,9 @@ export class ApiService {
   public deleteUser(userID: string) {
     return this.http.get<user>(`${environment.apiUrl}User/DeleteUser/${userID}`)
   }
+  public deleteUserByUserDetail(userDetailID: string) {
+    return this.http.get<user>(`${environment.apiUrl}User/DeleteUserByUserDetailId/${userDetailID}`)
+  }
   public checkUser(username: string) {
     return this.http.get<user>(`${environment.apiUrl}User/CheckUser/${username}`, { responseType: 'text' as 'json' })
   }
@@ -43,8 +49,8 @@ export class ApiService {
   public getRoomById(roomID: string) {
     return this.http.get<room>(`${environment.apiUrl}Room/GetRoomByid/${roomID}`)
   }
-  public getRoomByNumber(roomNnumber: string) {
-    return this.http.get<room>(`${environment.apiUrl}Room/GetRoomByNumber/${roomNnumber}`)
+  public getRoomByNumber(roomNumber: string) {
+    return this.http.get<room>(`${environment.apiUrl}Room/GetRoomByNumber/${roomNumber}`)
   }
   public getRoomByRoomType(roomType: string) {
     return this.http.get<room>(`${environment.apiUrl}Room/GetRoomByType/${roomType}`)
@@ -63,5 +69,52 @@ export class ApiService {
   }
   public deleteRoom(roomID: string) {
     return this.http.get<room>(`${environment.apiUrl}Room/DeleteRoom/${roomID}`)
+  }
+
+
+
+  public getAllUserDetail() {
+    return this.http.get<userDetail>(`${environment.apiUrl}UserDetail/GetAllUserDetail`)
+  }
+  public getUserDetailById(userDetailID: string) {
+    return this.http.get<userDetail>(`${environment.apiUrl}UserDetail/GetUserDetailById/${userDetailID}`)
+  }
+  public getUserDetailByFirstName(userDetailName: string) {
+    return this.http.get<userDetail>(`${environment.apiUrl}UserDetail/GetUserDetailByFirstName/${userDetailName}`)
+  }
+  public getUserDetailByTelephone(userDetailName: string) {
+    return this.http.get<userDetail>(`${environment.apiUrl}UserDetail/GetUserDetailByTelephone/${userDetailName}`)
+  }
+  public createUserDetail(userDetail: userDetail) {
+    return this.http.post<userDetail>(`${environment.apiUrl}UserDetail/CreateUserDetail`, userDetail)
+  }
+  public editUserDetail(userDetailID: string, userDetail: userDetail) {
+    return this.http.put<userDetail>(`${environment.apiUrl}UserDetail/EditUserDetail/${userDetailID}`, userDetail)
+  }
+  public deleteUserDetail(userDetailID: string) {
+    return this.http.get<userDetail>(`${environment.apiUrl}UserDetail/DeleteUserDetail/${userDetailID}`)
+  }
+
+
+  public getAllInvoice() {
+    return this.http.get<invoice>(`${environment.apiUrl}Invoice/GetAllInvoice`)
+  }
+  public getInvoiceById(invoiceID: string) {
+    return this.http.get<invoice>(`${environment.apiUrl}Invoice/GetInvoiceById/${invoiceID}`)
+  }
+  public getInvoiceByNumber(number: string) {
+    return this.http.get<invoice>(`${environment.apiUrl}Invoice/GetInvoiceByNumber/${number}`)
+  }
+  public getInvoiceByStatus(status: string) {
+    return this.http.get<invoice>(`${environment.apiUrl}Invoice/GetInvoiceByStatus/${status}`)
+  }
+  public createInvoice(invoice: invoice) {
+    return this.http.post<invoice>(`${environment.apiUrl}Invoice/CreateInvoice`, invoice)
+  }
+  public editInvoice(invoiceID: string, invoice: invoice) {
+    return this.http.put<invoice>(`${environment.apiUrl}Invoice/EditInvoice/${invoiceID}`, invoice)
+  }
+  public deleteInvoice(invoiceID: string) {
+    return this.http.get<invoice>(`${environment.apiUrl}Invoice/DeleteInvoice/${invoiceID}`)
   }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { RoomComponent } from './components/room/room.component';
+import { UserDetailComponent } from './components/user-detail/user-detail.component';
+import { ManageUserComponent } from './components/manage-user/manage-user.component';
+import { InvoiceDataComponent } from './components/invoice-data/invoice-data.component';
 
 const routes: Routes = [
-  // { path: '', redirectTo: 'login-page',pathMatch: 'full' },
-  { path: 'room', component: RoomComponent }
+  { path: '', redirectTo: 'room',pathMatch: 'full' },
+  { path: 'room', component: RoomComponent },
+  { path: 'invoice-data', component: InvoiceDataComponent },
+  { path: 'user-detail', component: UserDetailComponent },
+  { path: 'manage-user', component: ManageUserComponent}
 ]
 
 @NgModule({
